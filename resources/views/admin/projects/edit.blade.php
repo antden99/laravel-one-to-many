c8e90b9e1a59d3ea8e300874bdf6f84ebdb5fddc
antden99 <antoniodenigris@icloud.com>
2024-05-24 12:47:24 +0200

    add changes in edit

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -14,14 +14,14 @@
         @endif
 
 
-        <form action="{{ route('admin.projects.update',$project) }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('admin.projects.update', $project) }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('PUT')
 
             <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
                 <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name"
-                    aria-describedby="nameHelpId" placeholder="Add name project" value="{{ old('name',$project->name) }}" />
+                    aria-describedby="nameHelpId" placeholder="Add name project" value="{{ old('name', $project->name) }}" />
                 <small id="nameHelpId" class="form-text text-muted">Type a name for this project</small>
                 @error('name')
                     <div class="text-danger py-3">
@@ -46,7 +46,7 @@
             <div class="mb-3">
                 <label for="description" class="form-label">Description</label>
                 <textarea class="form-control" name="description" id="description" rows="4" aria-describedby="descriptionHelpId"
-                    placeholder="Add project description">{{ old('description',$project->description) }}</textarea>
+                    placeholder="Add project description">{{ old('description', $project->description) }}</textarea>
                 <small id="descriptionHelpId" class="form-text text-muted">Type a description for this project</small>
                 @error('description')
                     <div class="text-danger py-3">
@@ -55,11 +55,24 @@
                 @enderror
             </div>
 
+            <div class="mb-3">
+                <label for="type_id" class="form-label">Type</label>
+                <select class="form-select form-select-lg" name="type_id" id="type_id">
+                    <option selected>Select a type</option>
+                    @foreach ($types as $type)
+                        <option value="{{ $type->id }}" {{ $type->id == old('type_id',$project->type_id) ? 'selected' : '' }}>
+                            {{ $type->name }}</option>
+                        <!--Attendione: inserisci il valore di defaoult se non ci sono errori di validazione. Se c'è un errore di validazione, allora utilizza il metodo old e recupera il primo parametro, se il primo parametro non c'è utilizza il valore di default come recupero-->
+                    @endforeach
+                </select>
+            </div>
+
 
             <div class="mb-3">
                 <label for="start_date" class="form-label">Start Date</label>
                 <input type="date" class="form-control @error('start_date') is-invalid @enderror" name="start_date"
-                    id="start_date" aria-describedby="startDateHelpId" value="{{ old('start_date',$project->start_date) }}" />
+                    id="start_date" aria-describedby="startDateHelpId"
+                    value="{{ old('start_date', $project->start_date) }}" />
                 <small id="startDateHelpId" class="form-text text-muted">Select the start date for this project</small>
                 @error('start_date')
                     <div class="text-danger py-3">
@@ -72,7 +85,7 @@
             <div class="mb-3">
                 <label for="end_date" class="form-label">End Date</label>
                 <input type="date" class="form-control @error('end_date') is-invalid @enderror" name="end_date"
-                    id="end_date" aria-describedby="endDateHelpId" value="{{ old('end_date',$project->end_date) }}" />
+                    id="end_date" aria-describedby="endDateHelpId" value="{{ old('end_date', $project->end_date) }}" />
                 <small id="endDateHelpId" class="form-text text-muted">Select the end date for this project</small>
                 @error('end_date')
                     <div class="text-danger py-3">
