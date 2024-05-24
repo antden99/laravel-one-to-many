10d4067ebd5c890862e07e1fab4baa9ac136a59a
antden99 <antoniodenigris@icloud.com>
2024-05-24 15:09:32 +0200

    add type.index

---
+++ resources/views/admin/types/index.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.admin')
+
+
+@section('content')
+    <div class="container">
+        <div class="row">
+
+
+            @if (session('success'))
+                <div class="alert alert-success alert-dismissible fade show py-4" role="alert">
+                    {{ session('success') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+
+
+            <a href="#" class="p-4"><button class="btn btn-primary">Add
+                    Type</button></a>
+            <div class="col">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>TYPE NAME</th>
+                            <th>TYPE-SLUG</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        @foreach ($types as $type)
+                            <tr>
+
+                                <td>{{ $type->id }}</td>
+
+                                <td>{{ $type->name }}</td>
+                                <td>{{ $type->slug }}</td>
+                                <td>
+                                    <a href="{{ route('admin.types.show', $type) }}" class="btn btn-dark">
+                                        <i class="fa-regular fa-eye"></i></a>
+                                </td>
+                                <td>
+                                    <a href="{{ route('admin.types.edit', $type) }}" class="btn btn-dark">
+                                        <i class="fa-solid fa-pen-nib"></i></a>
+                                </td>
+                                <td>
+                                    <!-- Modal trigger button -->
+                                    <button type="button" class="btn btn-danger " data-bs-toggle="modal"
+                                        data-bs-target="#modalId-{{ $type->id }}">
+                                        <i class="fa-solid fa-trash-can"></i>
+                                    </button>
+
+                                    <!-- Modal Body -->
+                                    <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                                    <div class="modal fade" id="modalId-{{ $type->id }}" tabindex="-1"
+                                        data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+                                        aria-labelledby="modalTitleId" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                                            role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="modalTitleId">
+                                                        Be careful, you are eliminating the type from your list!!
+                                                    </h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">You are deleting the type {{ $type->title }}
+                                                    permanently!</div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-bs-dismiss="modal">
+                                                        Cancel <!--Annulla la modale-->
+                                                    </button>
+                                                    <form action="{{ route('admin.types.destroy', $type) }}" method="post">
+                                                        @csrf <!--ricorda di aggiungere sempre il token univoco-->
+                                                        @method('DELETE')
+                                                        <!--aggiungi sempre method 'delete' per indicare che questo form post è di tipo delete-->
+                                                        <button type="submit" class="btn btn-danger">
+                                                            <i class="fa-solid fa-trash-can"></i></button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\DashboardController; //controlla sempre che il controller sia importato correttamente
 use App\Http\Controllers\Admin\ProjectController; //controlla sempre che il controller sia importato correttamente per riga 32
+use App\Http\Controllers\TypeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
@@ -30,6 +31,8 @@ Route::middleware(['auth', 'verified'])
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard'); //qui praticamente sto dicendo che quando c'è /dashboard nell'url il controller gestisce il metodo index e ritorna una pagina, ho tolto '/dashboard' e messo solo '/', perchè in questo modo quando sarò autenticato, con solo admin trovero questa rotta
 
         Route::resource('/projects', ProjectController::class); //adesso questo admin/projects verrà gestito dal controller di Project in index in questo caso
+        Route::resource('/types', TypeController::class); //adesso questo admin/projects verrà gestito dal controller di Project in index in questo caso
+
 });
 
 Route::middleware('auth')->group(function () {
